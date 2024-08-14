545d1789c084798dc326c6f5524c02dab438518d
tajshuvoo <tajshuvo75@gmail.com>
2024-08-14 17:17:38 +0600

    djbhf

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["@babel/plugin-transform-private-property-in-object"],
+  plugins: [
+    "@babel/plugin-transform-private-property-in-object",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-jsx",
+    "@babel/plugin-proposal-class-properties"
+  ],
 };
