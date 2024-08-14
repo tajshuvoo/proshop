de236b6791f5ac72934075da855173016aa9a1c0
tajshuvoo <tajshuvo75@gmail.com>
2024-08-14 15:52:23 +0600

    Add existing project files to Git  h w d d 1

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     presets: ["@babel/preset-env", "@babel/preset-react"],
     plugins: ["@babel/plugin-proposal-private-property-in-object"],
+    plugins: ["@babel/plugin-transform-private-property-in-object"]
   };
